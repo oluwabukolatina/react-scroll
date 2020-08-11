a8229b75b315e01b27760141c02961bfa319758a
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-08-11 12:19:35 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
-import axios from 'axios'
 import Posts from "./components/Posts";
 import InfiniteScroll from 'react-infinite-scroll-component';
-// import InfiniteScroll from 'react-infinite-scroller';
+import {usePosts} from "./hooks/usePosts";
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [perPage, setPerPage] = useState(10);
-  const [loading, setLoading] = useState(false)
-const [currentPost, setCurrentPost] = useState([])
-  useEffect( ()=> {
-    const fetchPosts = async () => {
-      setLoading(true)
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
-    if(response.data){
-      setPosts(response.data);
-      setLoading(false)
-      setCurrentPost(response.data.slice(0, perPage))
-    }
-    };
-    fetchPosts();
-    // const update = (p) => {
-    //   console.log(p)
-    //   const indexOfLastPost = p * perPage;
-    //   const indexOfFirstPost = indexOfLastPost - perPage;
-    //   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
-    //   // console.log(currentPost.concat(currentPosts))
-    //   setCurrentPost(currentPost.concat(currentPosts))
-    //   // console.log(currentPost)
-    // }
-    // update(currentPage)
-
-
-  }, [])
-
-
+  const {posts, currentPost, getPosts} = usePosts();
   const getMore = () => {
-    // setCurrentPage(currentPage + 1)
   };
   window.onscroll = function() {
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-      setCurrentPage(currentPage + 1);
-      const indexOfLastPost = currentPage * perPage;
-      const indexOfFirstPost = indexOfLastPost - perPage;
-      setCurrentPost(currentPost.concat(posts.slice(indexOfFirstPost, indexOfLastPost)))
+     getPosts()
     }
   };
   return (
---
+++ src/hooks/usePosts.js
@@ -0,0 +1,29 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+export const usePosts = () => {
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [perPage] = useState(10);
+    const [currentPost, setCurrentPost] = useState([])
+    const [currentPage, setCurrentPage] = useState(1);
+    useEffect( ()=> {
+        const fetchPosts = async () => {
+            setLoading(true)
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
+            if(response.data){
+                setPosts(response.data);
+                setLoading(false)
+                setCurrentPost(response.data.slice(0, perPage))
+            }
+        };
+        fetchPosts();
+    }, [perPage]);
+    const getPosts = () => {
+        setCurrentPage(currentPage + 1);
+        const indexOfLastPost = (currentPage+1) * perPage;
+        const indexOfFirstPost = indexOfLastPost - perPage;
+        setCurrentPost(currentPost.concat(posts.slice(indexOfFirstPost, indexOfLastPost)))
+    };
+    return {posts, currentPost, currentPage, perPage, loading, getPosts}
+}
